f9d6419abfd424048dc65a5043634aac190b52a5
Jared Halpern <jared@bytemason.com>
2016-02-25 11:28:52 -0500

    Initial Commit

---
+++ AnchorTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnchorTable
+//
+//  Created by Jared Halpern on 2/25/16.
+//  Copyright © 2016 Jared Halpern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnchorTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnchorTable
+//
+//  Created by Jared Halpern on 2/25/16.
+//  Copyright © 2016 Jared Halpern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
